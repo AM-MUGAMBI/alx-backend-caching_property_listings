4130b39e6f5bd194cc704b5fe7efe190135b0250
AM-MUGAMBI <amugambi370@gmail.com>
2025-10-29 22:41:20 +0300

    get_reddis'

--- properties/utils.py
+++ properties/utils.py
@@ -12,29 +12,3 @@ def get_redis_cache_metrics():
     hit_ratio = hits / (hits + misses)  # calculates hit ratio directly
 
     return hit_ratio
-from django.core.cache import cache
-from django_redis import get_redis_connection
-from .models import Property
-import logging
-
-logger = logging.getLogger(__name__)
-
-def get_all_properties():
-    properties = cache.get('all_properties')
-    if not properties:
-        properties = list(Property.objects.all().values(
-            "id", "title", "description", "price", "location", "created_at"
-        ))
-        cache.set('all_properties', properties, 3600)
-    return properties
-
-def get_redis_cache_metrics():
-    redis_conn = get_redis_connection("default")
-    info = redis_conn.info("stats")
-
-    hits = info.get("keyspace_hits", 0)
-    misses = info.get("keyspace_misses", 0)
-    hit_ratio = hits / (hits + misses)  # simple calculation
-
-    logger.info(f"Redis cache hits: {hits}, misses: {misses}, hit ratio: {hit_ratio:.2f}")
-    return hit_ratio
